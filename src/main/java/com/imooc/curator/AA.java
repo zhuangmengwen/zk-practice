8c000ae0ac211042b228ecc4192a91210349b859
zhuangmengwen <zhuangmengwen@sunline.cn>
2020-12-11 17:55:26 +0800

    AAA

--- src/main/java/com/imooc/curator/AA.java
+++ src/main/java/com/imooc/curator/AA.java
@@ -5,6 +5,8 @@ public class AA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("这是新增的");
+		System.out.println("这是新增的");
+		System.out.println("这是新增的");
 	}
 
 }
